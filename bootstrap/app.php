b49aafef0ee226d8362cc7741844ee6024446d34
Dan Rovito <danrovito@gmail.com>
2016-11-11 09:49:23 -0500

    adding eloquent db

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,11 +7,31 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = new \Slim\App([
 	'settings' => [
-		'displayErrorDetails' => true,		
-	]
+		'displayErrorDetails' => true,	
+		'db' => [
+			'driver' => 'mysql',
+			'host' => 'localhost',
+			'database' => 'temp',
+			'username' => 'homestead',
+			'password' => 'secret',
+			'charset' => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix' => '',
+		]	
+	],
+	
 ]);
 
 $container = $app->getContainer();
+
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function($container) use ($capsule) {
+	return $capsule;
+};
 
 $container['view'] = function($container){
 	$view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
