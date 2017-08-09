263f106ba192d6e1752ad7d5e4c93c425c6d6c55
Dan Rovito <danrovito@gmail.com>
2017-08-09 14:23:38 +0000

    Apply fixes from StyleCI

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -4,17 +4,17 @@ namespace App\Controllers;
 
 class Controller
 {
-	protected $container;
+    protected $container;
 
-	public function __construct($container)
-	{
-		$this->container = $container;
-	}
+    public function __construct($container)
+    {
+        $this->container = $container;
+    }
 
-	public function __get($property)
-	{
-		if ($this->container->{$property}) {
-			return $this->container->{$property};
-		}
-	}
+    public function __get($property)
+    {
+        if ($this->container->{$property}) {
+            return $this->container->{$property};
+        }
+    }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,49 +2,48 @@
 
 session_start();
 
-
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 $app = new \Slim\App([
-	'settings' => [
-		'displayErrorDetails' => true,
-		'db' => [
-			'driver' => 'mysql',
-			'host' => 'localhost',
-			'database' => 'circle',
-			'username' => 'root',
-			'password' => null,
-			'charset' => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix' => '',
-		]
-	],
+    'settings' => [
+        'displayErrorDetails' => true,
+        'db'                  => [
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'circle',
+            'username'  => 'root',
+            'password'  => null,
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ],
+    ],
 
 ]);
 
 $container = $app->getContainer();
 
-$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule = new \Illuminate\Database\Capsule\Manager();
 $capsule->addConnection($container['settings']['db']);
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
 
-$container['db'] = function($container) use ($capsule) {
-	return $capsule;
+$container['db'] = function ($container) use ($capsule) {
+    return $capsule;
 };
 
-$container['view'] = function($container){
-	$view = new \Slim\Views\Twig(__DIR__ . '/../resources/views', [
-		'cache' => false,
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig(__DIR__.'/../resources/views', [
+        'cache' => false,
 
-	]);
+    ]);
 
-	$view->addExtension(new \Slim\Views\TwigExtension(
-		$container->router,
-		$container->request->getUri()
-	));
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $container->router,
+        $container->request->getUri()
+    ));
 
-	return $view;
+    return $view;
 };
 
-require __DIR__ . '/../app/routes.php';
+require __DIR__.'/../app/routes.php';
