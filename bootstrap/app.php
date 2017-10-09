3df1092fbaee78d31ff281a3a775ee0d78518da5
Ognjen <ognjen@heliozgroup.com>
2017-10-09 08:27:49 +0200

    Move db config to a config file

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,16 +9,7 @@ require __DIR__.'/../vendor/autoload.php';
 $app = new \Slim\App([
     'settings' => [
         'displayErrorDetails' => getenv('DEBUG'),
-        'db'                  => [
-            'driver'    => getenv('DB_DRIVER'),
-            'host'      => getenv('DB_HOST', '127.0.0.1'),
-            'database'  => getenv('DB_NAME'),
-            'username'  => getenv('DB_USER'),
-            'password'  => getenv('DB_PASSWORD'),
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
-        ],
+        'db'                  => require __DIR__.'/../config/database.php',
     ],
 
 ]);
